5f74b21d3800c94754b602c95fa83fd25d6ee1d5
Jorge <jorgegabriel.dev@gmail.com>
2026-05-29 20:31:19 -0300

    validacao do nome do titulo

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,3 +1,13 @@
-function fnValidacao(){
-    alert ("Sou uma função");
+function fnNomeTitulo(){
+    const campoTituloFilme = document.getElementById('tituloFilme');
+
+    if(campoTituloFilme.value.trim() == ""){
+        return console.log("Preencha o campo Titulo");
+    }
+
+    if(campoTituloFilme.value.length > 200){
+        console.log("Numero máximo atingido");
+    }
+
+    console.log(campoTituloFilme.value);
 }
